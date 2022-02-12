5fe671012a2f65b424b1b7b32843c0824b620605
ezequielpato <ezequiel.salomon@gmail.com>
2022-02-12 14:40:31 -0300

    Se crea archivo array de pdfs

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,9 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = 'http://localhost:3000/api';
 
-export const URL_IMAGE = "http://localhost:3000";
+export const URL_IMAGE = 'http://localhost:3000';
 
-export const URL_DELETE_IMAGE = "public";
+export const URL_DELETE_IMAGE = 'public';
 
-export const URL_SAVE_IMAGE = "./public";
-export const URL_SAVE_PDF = "./public";
-export const URL_DOWNLOAD = "./public";
+export const URL_SAVE_IMAGE = './public';
+export const URL_SAVE_PDF = './public';
+export const URL_DOWNLOAD = './public';
---
+++ utils/pdfs.js
@@ -0,0 +1,196 @@
+import { URL_PDF } from '../config/constants';
+
+export const pdfs = [
+	{
+		id: 1,
+		title: 'Analisis Tecnico',
+		files: [
+			{
+				id: 1,
+				pdf: '01 Analisis Tecnico y Velas Japonesas Gregorio Hernandez Jimenez',
+				ruta: `${URL_PDF}/01_analisis_tecnico/01_Analisis_Tecnico_y_Velas_Japonesas_Gregorio_Hernandez_Jimenez.pdf`,
+			},
+			{
+				id: 2,
+				pdf: '02 Analisis Tecnico de los Mercados Financi',
+				ruta: `${URL_PDF}/01_analisis_tecnico/02_Analisis_Tecnico_de_los_Mercados_Financi.pdf`,
+			},
+			{
+				id: 3,
+				pdf: '03  Curso  de  Analisis  Tecnico  Juan Urraca',
+				ruta: `${URL_PDF}/01_analisis_tecnico/03_Curso_de_Analisis_Tecnico_Juan Urraca.pdf`,
+			},
+			{
+				id: 4,
+				pdf: '04 El Libro Blanco de Trading Javier Pena',
+				ruta: `${URL_PDF}/01_analisis_tecnico/04_El_Libro_Blanco_de_Trading_Javier_Pena.pdf`,
+			},
+			{
+				id: 5,
+				pdf: '05 El Metodo Wickoff de Trading Enrique Diaz Valdecantos',
+				ruta: `${URL_PDF}/01_analisis_tecnico/05_El Metodo_Wickoff_de_Trading_Enrique_Diaz_Valdecantos.pdf`,
+			},
+			{
+				id: 6,
+				pdf: '06 Guia Basica de Analisis Chartista Amar Daryanani',
+				ruta: `${URL_PDF}/01_analisis_tecnico/06_Guia_Basica_de_Analisis_Chartista_Amar_Daryanani.pdf`,
+			},
+			{
+				id: 7,
+				pdf: '07 Guia de Patrones Armonicos Susana Vazquez',
+				ruta: `${URL_PDF}/01_analisis_tecnico/07_Guia_de_Patrones_Armonicos_Susana_Vazquez.pdf`,
+			},
+			{
+				id: 8,
+				pdf: '08 Leyendo Graficas Vela a Vela Al Brooks',
+				ruta: `${URL_PDF}/01_analisis_tecnico/08_Leyendo_Graficas_Vela_a_Vela_Al_Brooks.pdf`,
+			},
+			{
+				id: 9,
+				pdf: '09 Manual de Velas Japonesas Recopilacion',
+				ruta: `${URL_PDF}/01_analisis_tecnico/09_Manual_de_Velas_Japonesas_Recopilacion.pdf`,
+			},
+			{
+				id: 10,
+				pdf: '10 Salud Mental para Traders Norman Hallett',
+				ruta: `${URL_PDF}/01_analisis_tecnico/10_Salud_Mental_para_Traders_Norman_Hallett.pdf`,
+			},
+			{
+				id: 11,
+				pdf: '11 The Ultimate Guide to Bitcoin for Beginners 1',
+				ruta: `${URL_PDF}/01_analisis_tecnico/11_The_Ultimate_Guide_to_Bitcoin_for_Beginners_1.pdf`,
+			},
+			{
+				id: 12,
+				pdf: '12 Trading en 10 pasos Pedrop Lopez',
+				ruta: `${URL_PDF}/01_analisis_tecnico/12_Trading_en_10_pasos_Pedrop_Lopez.pdf`,
+			},
+			{
+				id: 13,
+				pdf: '13 TRADING EN LA ZONA',
+				ruta: `${URL_PDF}/01_analisis_tecnico/13_TRADING_EN_LA_ZONA.pdf`,
+			},
+			{
+				id: 14,
+				pdf: '14 Una moneda criptográfica',
+				ruta: `${URL_PDF}/01_analisis_tecnico/14_Una_moneda_criptográfica.pdf`,
+			},
+			{
+				id: 15,
+				pdf: '15 VIVIR DEL TRADING',
+				ruta: `${URL_PDF}/01_analisis_tecnico/15_VIVIR_DEL_TRADING.pdf`,
+			},
+		],
+	},
+
+	{
+		id: 2,
+		title: 'Robert Kiyosaki',
+		files: [
+			{
+				id: 1,
+				pdf: '01 Analisis Tecnico y Velas Japonesas Gregorio Hernandez Jimenez',
+				ruta: `${URL_PDF}/02_robert_kiyosaki/01_El_cuadrante_flujo_del_dinero_Robert_Kiyosaki.pdf`,
+			},
+			{
+				id: 2,
+				pdf: '02 Analisis Tecnico de los Mercados Financi',
+				ruta: `${URL_PDF}/02_robert_kiyosaki/02_Escuela_de_Negocios_Robert_Kiyosaki_1.pdf`,
+			},
+			{
+				id: 3,
+				pdf: '03 Guia para Invertir Robert Kiyosaki',
+				ruta: `${URL_PDF}/02_robert_kiyosaki/03_Guia_para_Invertir_Robert_Kiyosaki.pdf`,
+			},
+			{
+				id: 4,
+				pdf: '04 Nino Rico Nino Listo Robert Kiyosaki',
+				ruta: `${URL_PDF}/02_robert_kiyosaki/04_Nino_Rico_Nino_Listo_Robert_Kiyosaki.pdf`,
+			},
+			{
+				id: 5,
+				pdf: '05 Padre rico padre pobre Robert Kiyosaki',
+				ruta: `${URL_PDF}/02_robert_kiyosaki/05_Padre_rico_padre_pobre_Robert_Kiyosaki.pdf`,
+			},
+			{
+				id: 6,
+				pdf: '06 Retirate Joven y Rico Robert Kiyosaki',
+				ruta: `${URL_PDF}/02_robert_kiyosaki/06_Retirate_Joven_y_Rico_Robert_Kiyosaki.pdf`,
+			},
+		],
+	},
+
+	{
+		id: 3,
+		title: 'Ondas de Elliot',
+		files: [
+			{
+				id: 1,
+				pdf: '01 Dominando la Onda de Elliot Glenn Neely',
+				ruta: `${URL_PDF}/03_ondas_elliot/01_Dominando_la_Onda_de_Elliot_Glenn_Neely.pdf`,
+			},
+			{
+				id: 2,
+				pdf: '02 La Onda de Elliot Arthur Hamilton Bolton',
+				ruta: `${URL_PDF}/03_ondas_elliot/02_La_Onda_de_Elliot_Arthur_Hamilton_Bolton.pdf`,
+			},
+			{
+				id: 3,
+				pdf: '03 Manual Basico del Principio de la Onda de Elliott',
+				ruta: `${URL_PDF}/03_ondas_elliot/03_Manual_Basico_del_Principio_de_la_Onda_de_Elliott.pdf`,
+			},
+			{
+				id: 4,
+				pdf: '04 Ondas de Elliot Pablo Desiderio',
+				ruta: `${URL_PDF}/03_ondas_elliot/04_Ondas_de_Elliot_Pablo_Desiderio.pdf`,
+			},
+			{
+				id: 5,
+				pdf: '05 Ondas de Elliot en Pocas Palabras Curso Forex',
+				ruta: `${URL_PDF}/03_ondas_elliot/05_Ondas_de_Elliot_en_Pocas_Palabras_Curso_Forex.pdf`,
+			},
+			{
+				id: 6,
+				pdf: '06 Principios de Ondas de Elliot Frost y Pretcher',
+				ruta: `${URL_PDF}/03_ondas_elliot/06_Principios_de_Ondas_de_Elliot_Frost_y_Pretcher.pdf`,
+			},
+			{
+				id: 7,
+				pdf: '07 Teoría de las Ondas de Elliot Jose Carlos Estevez',
+				ruta: `${URL_PDF}/03_ondas_elliot/07_Teoría_de_las_Ondas_de_Elliot_Jose_Carlos_Estevez.pdf`,
+			},
+			{
+				id: 8,
+				pdf: '08 Teoria de Ondas de Elliot Prognosis Software',
+				ruta: `${URL_PDF}/03_ondas_elliot/08_Teoria_de_Ondas_de_Elliot_Prognosis_Software.pdf`,
+			},
+			{
+				id: 9,
+				pdf: '09 Teoria y Practica Moderna de las Ondas de Elliot Oscar Cagigas',
+				ruta: `${URL_PDF}/03_ondas_elliot/09_Teoria_y_Practica_Moderna_de_las_Ondas_de_Elliot_Oscar_Cagigas.pdf`,
+			},
+		],
+	},
+
+	{
+		id: 4,
+		title: 'Fibonacci',
+		files: [
+			{
+				id: 1,
+				pdf: 'Fibonacci 01',
+				ruta: `${URL_PDF}/04_fibonacci/Fibonacci_01.pdf`,
+			},
+			{
+				id: 2,
+				pdf: 'Fibonacci 02',
+				ruta: `${URL_PDF}/04_fibonacci/Fibonacci_02.pdf`,
+			},
+			{
+				id: 3,
+				pdf: 'Fibonacci 03',
+				ruta: `${URL_PDF}/04_fibonacci/Fibonacci_03.pdf`,
+			},
+		],
+	},
+];
